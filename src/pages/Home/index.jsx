15cd42626495b62a9b1f9c8777f6ce1eb5ab5936
Felipe Coelho <felipeaacoelho.com>
2023-10-08 22:11:26 -0300

    ajusted scroll page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,70 +2,73 @@ import { HeaderMobile } from "../../components/Header/Mobile";
 import { HeaderDesktop } from "../../components/Header/Desktop";
 import { Footer } from "../../Components/Footer";
 import { Container } from "./styles";
-import home from '../../assets/homeImg.svg'
+import home from "../../assets/homeImg.svg";
 
-export function Home(){
+export function Home() {
+  return (
+    <Container>
+      <HeaderDesktop />
+      <HeaderMobile />
+      <main>
+        <div id="display">
+          <div id="banner"></div>
+          <img id="imgHome" src={home} alt="" />
+          <span id="presentation">
+            <h3>Sabores inigualáveis</h3>
+            <p>Sinta o cuidado do preparo com ingredientes selecionados.</p>
+          </span>
+        </div>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+        <h1>
+          <span>Sabores inigualáveis</span>
+        </h1>
+      </main>
 
-    return(
-        <Container>
-            <HeaderDesktop/>
-            <HeaderMobile/>
-            <div id="banner"></div>
-            <img id="imgHome" src={home} alt="" />
-            <span id="presentation">
-                <h3>Sabores inigualáveis</h3>
-                <p>Sinta o cuidado do preparo com ingredientes selecionados.</p>
-            </span>
-            <main>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                <h1>
-                    <span>Sabores inigualáveis</span>
-                </h1>
-                
-            </main>
-    
-            <Footer/>
-        </Container>
-    )
+      <Footer />
+    </Container>
+  );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,47 +1,68 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+  position: relative;
+
+  header {
+    position: fixed;
+    z-index: 2;
+    background-color: ${({ theme }) => theme.COLORS.DARK._400};
+  }
+
+  main {
     display: flex;
     flex-direction: column;
     align-items: center;
-    /* height: 100vh; */
-    justify-content: right;
 
-    position: relative;
+    margin-top: 12.5rem;
+    margin-bottom: 3rem;
 
-    #banner{
-        background: linear-gradient(200deg, rgba(2,0,36,1) 0%, rgba(9,30,38,1) 35%);
-        /* width: fit-content; */
+    width: 100%;
+    overflow-y: auto;
 
-        width: clamp(31.5rem, 20% + 80vw , 112rem);
-        /* height: 26rem; */
+    #display {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: right;
+      width: 100%;
+      margin-top: 2.2rem;
+
+
+      #banner {
+        background: linear-gradient(
+          200deg,
+          rgba(2, 0, 36, 1) 0%,
+          rgba(9, 30, 38, 1) 35%
+        );
+
+        width: clamp(31.5rem, 20% + 80vw, 112rem);
         margin-top: 2.4rem;
         z-index: -1;
+      }
 
-    }
-
-    > #imgHome{
+      #imgHome {
         position: absolute;
         left: 0;
-        top: 12rem;
-        
-    }
-    #presentation{
-
+        top: 14rem;
+      }
+      #presentation {
         position: absolute;
         right: 0;
-        
+
         z-index: 1;
 
         padding-top: 4.5rem;
 
-        p{
-            font-size: 1.2rem;
+        p {
+          font-size: 1.2rem;
         }
+      }
     }
-
-    main{
-        /* height: 100%; */
-    }
-
-`
+  }
+`;
